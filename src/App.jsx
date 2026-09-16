27ace3fad1bc69a998a6c9f3a0efe2c2047016ba
Nam Trinh <nht35@drexel.edu>
2026-09-15 21:45:34 -0400

    initialize and config the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-amber-400">
+        Welcome my My porfolio
+      </h1>
+    </div>
+  );
+};
+
+export default App;
